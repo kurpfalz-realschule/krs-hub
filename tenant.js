3fd440138cf852604654d7780d3215da4f1897ad
Norbert Kotzan <kotzan@realschule-schriesheim.de>
2026-07-07 08:29:05 +0200

    fix: Kalender-Link auf Outlook umgestellt, Dateiablage (Nextcloud) in Sidebar ergänzt

--- tenant.js
+++ tenant.js
@@ -8,5 +8,6 @@
 window.KRS_TENANT = {
   EMAIL_DOMAIN: 'realschule-schriesheim.de',
   HOMEPAGE_URL: 'https://realschule-schriesheim.de/',
-  KALENDER_URL: 'https://realschule-schriesheim.de/terminuebersicht-im-kalenderformat/'
+  KALENDER_URL: 'https://outlook.office365.com/owa/calendar/a95005ab7be44772829ded01908dd047@realschule-schriesheim.de/c28240066c4043798c40398ea699cee116822421613853451472/calendar.html',
+  NEXTCLOUD_URL: 'https://cloud.realschule-schriesheim.de'
 };
